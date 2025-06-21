97f98cc95cde8040737505077113de7a42823167
Alkumist <ponyyouman@gmail.com>
2025-06-21 16:21:42 -0600

    initial github upload

---
+++ data-updates.lua
@@ -0,0 +1,20 @@
+------------------------------------------ Items
+data.raw["upgrade-item"]["upgrade-planner"].icon								=		"__alk-toolbar-substitutes__/graphics/colored-icons/upgrade-green.png"
+data.raw["deconstruction-item"]["deconstruction-planner"].icon	=		"__alk-toolbar-substitutes__/graphics/colored-icons/deconstruction-red.png"
+data.raw["blueprint"]["blueprint"].icon													=		"__alk-toolbar-substitutes__/graphics/colored-icons/blueprint-blue.png"
+data.raw["copy-paste-tool"]["copy-paste-tool"].icon							=		"__alk-toolbar-substitutes__/graphics/colored-icons/copy-paste-tool-blue.png"
+data.raw.item["copper-wire"].icon																=		"__alk-toolbar-substitutes__/graphics/new-copper-wire-x56.png"
+------------------------------------------ Toolbar
+data.raw.shortcut["copy"].icon																	=		"__alk-toolbar-substitutes__/graphics/copy-x56.png"
+data.raw.shortcut["give-copper-wire"].icon											=		"__alk-toolbar-substitutes__/graphics/new-copper-wire-x56.png"
+data.raw.shortcut["give-red-wire"].icon													=		"__base__/graphics/icons/shortcut-toolbar/mip/new-red-wire-x56.png"
+data.raw.shortcut["give-green-wire"].icon												=		"__base__/graphics/icons/shortcut-toolbar/mip/new-green-wire-x56.png"
+
+data.raw.shortcut["copy"].small_icon														=		"__alk-toolbar-substitutes__/graphics/copy-x24.png"
+data.raw.shortcut["give-copper-wire"].small_icon								=		"__alk-toolbar-substitutes__/graphics/new-copper-wire-x24.png"
+data.raw.shortcut["give-red-wire"].small_icon										=		"__base__/graphics/icons/shortcut-toolbar/mip/new-red-wire-x24.png"
+data.raw.shortcut["give-green-wire"].small_icon									=		"__base__/graphics/icons/shortcut-toolbar/mip/new-green-wire-x24.png"
+------------------------------------------ Recolor Drag Select
+data.raw.shortcut["copy"].style = "blue"
+data.raw["copy-paste-tool"]["copy-paste-tool"].alt_select["border_color"] = {57, 156, 251}
+data.raw["utility-sprites"].default["down_arrow"].filename = "__alk-toolbar-substitutes__/graphics/down-arrow.png"
